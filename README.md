c68161906ca7de8bc66be6ad3d4356427bde93cf
olivia nonye <oliviaagabi@gmail.com>
2022-09-24 13:55:38 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # introdution
 # introdution
+# introdution
---
+++ introduction.js
@@ -0,0 +1,10 @@
+/*
+Intro to javaScript
+*/
+// Data Types
+// Numbers: are digits. E.g: 0-9, 1.5, 3.14
+var num = 10;
+var num2 = 5.6;
+var num3 = -190;
+//string: are characters. E.g:"H"
+var 
